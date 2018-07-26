0d01fece5280f7a4ffca08904090e41e311cabc9
code-ee <copatterson@csumb.edu>
2018-07-26 14:08:46 -0700

    initialized variables for BillProcessor

--- src/main/java/cpatterson/projects/solo/distribilltion/process/BillProcessor.java
+++ src/main/java/cpatterson/projects/solo/distribilltion/process/BillProcessor.java
@@ -1,39 +1,52 @@
 package cpatterson.projects.solo.distribilltion.process;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import org.json.JSONObject;
 
+import cpatterson.projects.solo.distribilltion.model.Payer;
+
 public class BillProcessor {
-	private JSONObject incomes;
-	private JSONObject bills;
-	
 	private double totalBills;
 	private double totalIncome;
-	private double ratio;
-	
+	private List<Payer> people;
+
 	public BillProcessor(JSONObject parameters) {
 		super();
-		this.incomes = (JSONObject) parameters.get("incomes");
-		this.bills = (JSONObject) parameters.get("bills");
-		totalBills = 0;
-		totalIncome = 0;
-		ratio = 0;
+		this.totalBills = calculateTotal(parameters.getJSONObject("bills"));
+		this.totalIncome = calculateTotal(parameters.getJSONObject("people"));
+		this.people = getPayers(parameters.getJSONObject("people"));
 	}
 	
 	public void run() {
 		System.out.println("This worked so far!");
 	}
 	
-	public double addAllBills(JSONObject bills) {
-		Iterator<?> keys = bills.keys();
+	public double calculateTotal(JSONObject jsonObject) {
+		double total = 0;
+		Iterator<?> keys = jsonObject.keys();
 
-		while( keys.hasNext() ) {
+		while (keys.hasNext()) {
 		    String key = (String)keys.next();
-		    if (bills.get(key) instanceof JSONObject) {
-
-		    }
+		    total += jsonObject.getDouble(key);
 		}
-		return 0;
+		return total;
 	}
+	
+	public ArrayList<Payer> getPayers(JSONObject incomes) {
+		ArrayList<Payer> payers = new ArrayList<Payer>();
+		double income;
+		Iterator<?> keys = incomes.keys();
+
+		while (keys.hasNext()) {
+		    String key = (String)keys.next();
+		    income = incomes.getDouble(key);
+		    payers.add(new Payer(key, income, (income/this.totalIncome)));
+		}
+		
+		return payers;
+	}
+
 }
